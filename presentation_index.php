a47ac6fc31841b84501245ccef0e643239723fe1
Chris Cargile <follybeachris@hotmail.com>
2020-04-10 01:18:31 +0000

    update presentation_index.php

--- presentation_index.php
+++ presentation_index.php
@@ -5,5 +5,6 @@ echo "<pre>";
   $var = @$_GET['q'] ;
   $trimmed = trim($var); //trim whitespace from the stored variable
 
-  $files = glob("./*.txt");
+  $files = glob("./*.{JPG,gif,png}", GLOB_BRACE);
+  var_dump(basename($files[0],".JPG"));
 ?>
